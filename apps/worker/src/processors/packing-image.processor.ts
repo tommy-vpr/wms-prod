efa2e1278c954af90139eea6b5e4a9a3935be809
tommy-vpr <tommy@cultivatedagency.com>
2026-02-18 15:17:00 -0800

    Revert GCP private key /n

--- apps/worker/src/processors/packing-image.processor.ts
+++ apps/worker/src/processors/packing-image.processor.ts
@@ -28,7 +28,7 @@ async function getStorage() {
     projectId: process.env.GCP_PROJECT_ID,
     credentials: {
       client_email: process.env.GCP_CLIENT_EMAIL,
-      private_key: process.env.GCP_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      private_key: process.env.GCP_PRIVATE_KEY,
     },
   });
 
